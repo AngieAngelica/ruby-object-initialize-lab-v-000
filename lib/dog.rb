eb2b8121fc713fdeb8568b19ea97a3dc420a68e1
Angie Kassab <anasab12@gmail.com>
2018-05-15 16:33:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,15 @@ class Dog
    def initialize (name)
     @name = name
   end
+  
+  def name = (name)
+    @name = name
+  end
+  
+  def name
+    @name
+    
+  end
 
 def initialize (breed)
   @breed = breed
